22740af87ac8004a028cc4ff64c15dc4654cc4a7
root <wariiyuanne@gmail.com>
2022-03-15 01:02:05 -0700

    printing 10 times in a nested while loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - program to print the lower case,
+ * alphabet 10 times,
+ * in a new line
+ * Return: Always 0 Success
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	while (i < 10)
+	{
+		ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
+
