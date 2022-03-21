0adbe22148375837c9e4b0695ebbc2b19bb40a63
root <wariiyuanne@gmail.com>
2022-03-21 15:44:15 -0700

    printing an array n times

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -9,16 +9,18 @@
  */
 void print_array(int *a, int n)
 {
-	for (int i = 0; i < n; i++)
+	int i;
+
+	for (i = 0; i < n; i++)
 	{
 		if (i == 0)
-		{
+	
 			printf("%d", a[i]);
-		}
+	
 		else
-		{
-			print(", %d", a[i]);
-		}
+	
+			printf(", %d", a[i]);
+	
 	}
 	printf("\n");
 }
