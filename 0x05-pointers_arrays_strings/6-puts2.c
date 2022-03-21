2f48be43f28e9f6c2738c48ded370b531c61b79e
root <wariiyuanne@gmail.com>
2022-03-21 14:26:49 -0700

    to print each character out of 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one character out of 2 of a string
+ * followed by a new line
+ * @str - string to print out the char from
+ */
+void puts2.c(char *str)
+{
+	int i, len;
+
+	len = 0;
+
+	while (s[len] != '\0')
+	{
+		s[len]++;
+	}
+
+	for (i = 0; i < len; i += 2)
+	{
+		_putchar(s[i]);
+	}
+
+	_putchar('\n');
+}
