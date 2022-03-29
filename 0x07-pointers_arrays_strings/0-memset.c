42277c2396091d5e345e2dd5d42b7808e6904f0d
root <wariiyuanne@gmail.com>
2022-03-29 03:26:41 -0700

    fumction to fit memory with constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,11 @@
 #include "main.h"
 
+/**
+ * _memset - function fills the first
+ * @n bytes of memory area pointed to by 
+ * @s with the constant byte b
+ * Returns a pointer to the memory area s
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int n = i;
@@ -8,5 +14,6 @@ char *_memset(char *s, char b, unsigned int n)
 	{
 		s[i] = b;
 	}
+
 	return (s);
 }
