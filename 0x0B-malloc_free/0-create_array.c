9dd813a18af02821c17908dd36ca3d78c7853dde
root <wariiyuanne@gmail.com>
2022-04-04 07:02:40 -0700

    creating an array using malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,7 +10,7 @@
  */
 char *create_array(unsigned int size, char c)
 {
-	char *n = (char *)malloc(n * sizeof(char n));
+	char *n = (char *) malloc(sizeof(char) * size);
 	unsigned int i;
 
 	if (size == 0)
