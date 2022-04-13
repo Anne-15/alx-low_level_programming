f1d0b468032616471bce5b60ef4341c8e092ee9e
Anne-15 <wariiyuanne@gmail.com>
2022-04-13 07:23:16 -0700

    function to search for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,7 +10,7 @@
  * Return index in the first element for which
  * the cmp function does not return 0
  */
-int int_array(int *array, int size, int (*cmp)(int))
+int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
