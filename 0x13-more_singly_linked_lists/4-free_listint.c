fb0f072d153bb191558eb439e594bc1c22957a30
Anne-15 <wariiyuanne@gmail.com>
2022-04-27 22:41:19 -0700

    function to free a node

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - function that frees a node
+ * @head: pointer to the list
+ *
+ * Return: null
+ *
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head)
+	{
+		temp = head-> next;
+		free(head);
+		head = temp;
+	}
+}
