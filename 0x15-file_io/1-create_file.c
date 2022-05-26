6da844fe62b3fef8de14afa45084d5d3dbc335b6
Anne-15 <wariiyuanne@gmail.com>
2022-05-26 01:51:50 -0700

    function to create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content)
 	{
-		while(text_content[j])
+		while (text_content[j])
 			j++;
 		i = write(fd, text_content, j);
 		if (i != j)
