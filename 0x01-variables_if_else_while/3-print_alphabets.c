e49e6edb2184e2a945bd84abb61880865867b4e8
Anne-15 <wariiyuanne@gmail.com>
2022-06-10 22:40:23 -0700

    changes

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,13 +7,9 @@ int main(void)
 	int sh;
 
 	for (sh = 'a'; sh <= 'z'; sh++)
-	{
 		putchar(sh);
-	}
-	for (sh = 'A'; sh<= 'Z''; sh++)
-	{
+	for (sh = 'A'; sh <= 'Z''; sh++)
 		putchar(sh);
-	}
 	putchar('\n');
 	return (0);
 }
