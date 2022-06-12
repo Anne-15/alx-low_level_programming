b7d0cf85bd0ec1e05e948d5dcced5f0e818a3833
Anne-15 <wariiyuanne@gmail.com>
2022-06-12 09:22:33 -0700

    changes

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,6 +1,10 @@
 #include "main.h"
 
 /**
+ * reverse_array - function that reverses the contents of an array
+ * of ints
+ * @a: array to be reversed
+ * @n: number of elements in the array
  */
 void reverse_array(int *a, int n)
 {
